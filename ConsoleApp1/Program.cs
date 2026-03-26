01983451ba3d6abe154b708765d50ad81f2e8b16
666yar666 <70371319+666yar666@users.noreply.github.com>
2026-03-26 12:09:46 +0200

    60

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System;
+
+namespace task1
+{
+    public delegate string TextOperation(string input);
+    
+    class Programm
+    {
+        static void Main()
+        {
+            string inputFile = "textPD25.txt";
+            string outputFile = "resultPD25.txt"; 
+
+            File.WriteAllText(outputFile, "");
+
+            if (!File.Exists(inputFile)) 
+            {
+                File.WriteAllText(inputFile, "C# is awesome. Good luck on the exam.");
+            }
+
+            ProcessFile(inputFile, outputFile, text => text.ToUpper());
+            ProcessFile(inputFile, outputFile, text => $"Кількість символів: {text.Length}");
+            ProcessFile(inputFile, outputFile, text => 
+            {
+                int wordCount = text.Split(new[] { ' ', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries).Length;
+                return $"Кількість слів: {wordCount}";
+            });
+            
+            Console.WriteLine("Успішно, результат записаний в файл resultPD25.txt"); 
+        } 
+
+        static void ProcessFile(string inputFilePath, string outputFilePath, TextOperation operation)
+        {
+            string text = File.ReadAllText(inputFilePath);
+            string result = operation(text);  
+            using (StreamWriter sw = new StreamWriter(outputFilePath, true))
+            {
+                sw.WriteLine("--- Результат операції ---");
+                sw.WriteLine(result);
+                sw.WriteLine();
+            }
+        }
+    } 
+} 
