f60c9734f9ee069365b5a88a38a250de5261058c
666yar666 <70371319+666yar666@users.noreply.github.com>
2026-03-27 18:29:20 +0200

    Add files via upload

---
+++ task5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task5
+{
+    class Logger
+    {
+        public Action<string> LogHandler;
+
+        public void Log(string message)
+        {
+            if (LogHandler != null)
+            {
+                LogHandler(message);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Logger logger = new Logger();
+
+            // Спочатку виводимо просто в консоль
+            logger.LogHandler = message => Console.WriteLine(message);
+            logger.Log("Перше повідомлення системи");
+
+            // Змінюємо поведінку "на льоту" - тепер у верхньому регістрі
+            logger.LogHandler = message => Console.WriteLine(message.ToUpper());
+            logger.Log("Друге повідомлення системи (великими літерами)");
+        }
+    }
+}
---
+++ task6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task6
+{
+    public delegate bool Validator(string text);
+
+    class Program
+    {
+        static Validator GetValidator(int minLength)
+        {
+            return text => text != null && text.Length >= minLength;
+        }
+
+        static void Main()
+        {
+            Validator loginValidator = GetValidator(3);
+            Validator passwordValidator = GetValidator(8);
+
+            Console.WriteLine("Перевірка логіна 'ab': " + loginValidator("ab"));
+            Console.WriteLine("Перевірка логіна 'admin': " + loginValidator("admin"));
+
+            Console.WriteLine("Перевірка пароля '123': " + passwordValidator("123"));
+            Console.WriteLine("Перевірка пароля 'qwerty12345': " + passwordValidator("qwerty12345"));
+        }
+    }
+}
